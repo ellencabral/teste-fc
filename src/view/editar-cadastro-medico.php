c8b389c3c5bf43f1de1371061c3024ce1a418062
Ellen <ellencabral2098@gmail.com>
2020-11-23 23:25:01 -0300

    aplicação de estilos no editar medico

--- src/view/editar-cadastro-medico.php
+++ src/view/editar-cadastro-medico.php
@@ -12,21 +12,22 @@
     $usuario = $medicoDAO->visualizaMedico($medico);
   }
   ?>
-
-  <h1>Editar médico</h1>
-
-  <form method='post' action='../controller/medico-controller.php'>
-      Nome
+  <div class="form-container flex-column">
+    <form class="flex-column" method='post' action='../controller/medico-controller.php'>
+      <h1>Editar médico</h1>
+      <label for ="nome">Nome</label>
       <input type='text' name='nome' value='<?php echo $usuario->getNome() ?>'/>
-      Senha antiga
+      <label for ="senhaantiga">Senha antiga</label>
       <input type='password' name='senhaantiga' placeholder='Insira a senha antiga'/>
-      Nova senha
+      <label for ="novasenha">Nova senha</label>
       <input type='password' name='novasenha' placeholder='Escolha uma nova senha forte e segura'/>
       <input type='hidden' name='id' value='<?php echo $usuario->getId() ?>'>
       
-      <button type='submit' name='atualiza'>Atualizar cadastro</button>
-  </form>
+      <button class='button' type='submit' name='atualiza'>Atualizar cadastro</button>
+    </form>
 
-  <a href='../index.php'>Voltar para a Página Inicial</a>
+    <a href='../index.php'>Voltar para a Página Inicial</a>
+  </div>
+</div>
 </body>
 </html>
